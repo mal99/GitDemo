3ad8f97f91cc8ca98781761b290b1490ea3b51f3
malathi <malathi.ara6@gmail.com>
2019-03-06 10:20:56 -0600

    Initial Commit

---
+++ src/main/java/gmracademy/App.java
@@ -0,0 +1,13 @@
+package gmracademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
